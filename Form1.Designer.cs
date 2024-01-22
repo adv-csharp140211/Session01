d9f145770071df5ab01ee4cd14787a7edc11b450
Pasha <pashaie@gmail.com>
2024-01-22 20:16:54 +0330

    init clac form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,19 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
+            labelA = new Label();
+            labelB = new Label();
+            textBoxA = new TextBox();
+            textBoxB = new TextBox();
+            buttonAdd = new Button();
+            buttonSub = new Button();
+            buttonMul = new Button();
+            buttonDiv = new Button();
+            label1 = new Label();
+            textBoxResult = new TextBox();
             SuspendLayout();
+            // 
+            // labelA
+            // 
+            labelA.AutoSize = true;
+            labelA.Location = new Point(48, 30);
+            labelA.Name = "labelA";
+            labelA.Size = new Size(15, 15);
+            labelA.TabIndex = 0;
+            labelA.Text = "A";
+            // 
+            // labelB
+            // 
+            labelB.AutoSize = true;
+            labelB.Location = new Point(48, 59);
+            labelB.Name = "labelB";
+            labelB.Size = new Size(14, 15);
+            labelB.TabIndex = 1;
+            labelB.Text = "B";
+            // 
+            // textBoxA
+            // 
+            textBoxA.Location = new Point(92, 27);
+            textBoxA.Name = "textBoxA";
+            textBoxA.Size = new Size(345, 23);
+            textBoxA.TabIndex = 2;
+            // 
+            // textBoxB
+            // 
+            textBoxB.Location = new Point(92, 56);
+            textBoxB.Name = "textBoxB";
+            textBoxB.Size = new Size(345, 23);
+            textBoxB.TabIndex = 3;
+            // 
+            // buttonAdd
+            // 
+            buttonAdd.Location = new Point(92, 85);
+            buttonAdd.Name = "buttonAdd";
+            buttonAdd.Size = new Size(65, 23);
+            buttonAdd.TabIndex = 4;
+            buttonAdd.Text = "➕";
+            buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // buttonSub
+            // 
+            buttonSub.Location = new Point(163, 85);
+            buttonSub.Name = "buttonSub";
+            buttonSub.Size = new Size(65, 23);
+            buttonSub.TabIndex = 5;
+            buttonSub.Text = "➖";
+            buttonSub.UseVisualStyleBackColor = true;
+            // 
+            // buttonMul
+            // 
+            buttonMul.Location = new Point(234, 85);
+            buttonMul.Name = "buttonMul";
+            buttonMul.Size = new Size(65, 23);
+            buttonMul.TabIndex = 6;
+            buttonMul.Text = "✖️";
+            buttonMul.UseVisualStyleBackColor = true;
+            // 
+            // buttonDiv
+            // 
+            buttonDiv.Location = new Point(305, 85);
+            buttonDiv.Name = "buttonDiv";
+            buttonDiv.Size = new Size(65, 23);
+            buttonDiv.TabIndex = 7;
+            buttonDiv.Text = "➗";
+            buttonDiv.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(48, 124);
+            label1.Name = "label1";
+            label1.Size = new Size(39, 15);
+            label1.TabIndex = 8;
+            label1.Text = "Result";
+            // 
+            // textBoxResult
+            // 
+            textBoxResult.Location = new Point(92, 121);
+            textBoxResult.Name = "textBoxResult";
+            textBoxResult.ReadOnly = true;
+            textBoxResult.Size = new Size(345, 23);
+            textBoxResult.TabIndex = 9;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(477, 184);
+            Controls.Add(textBoxResult);
+            Controls.Add(label1);
+            Controls.Add(buttonDiv);
+            Controls.Add(buttonMul);
+            Controls.Add(buttonSub);
+            Controls.Add(buttonAdd);
+            Controls.Add(textBoxB);
+            Controls.Add(textBoxA);
+            Controls.Add(labelB);
+            Controls.Add(labelA);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label labelA;
+        private Label labelB;
+        private TextBox textBoxA;
+        private TextBox textBoxB;
+        private Button buttonAdd;
+        private Button buttonSub;
+        private Button buttonMul;
+        private Button buttonDiv;
+        private Label label1;
+        private TextBox textBoxResult;
     }
 }
